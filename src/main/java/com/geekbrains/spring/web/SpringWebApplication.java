7d53aedd823e670916a81452059a41fcc772c099
CAM1989 <cherepov-a@mail.ru>
2022-08-25 09:27:36 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/spring/web/SpringWebApplication.java
@@ -0,0 +1,17 @@
+package com.geekbrains.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApplication {
+	// Домашнее задание:
+	// Реализовать перехватчик методов удаления и
+	// *Вывести пользователя, который залогинился
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebApplication.class, args);
+	}
+}
